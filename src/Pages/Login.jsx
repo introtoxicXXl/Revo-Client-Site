8e68dd7e76e1f34aa7ca96503ed0b5d0c3955130
introtoxicXXl <minhajulabedin648@gmail.com>
2023-12-05 02:06:56 +0600

    login page added

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,110 @@
+import { Helmet } from "react-helmet-async";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import useAuth from "../Hook/hook";
+import SocialUser from "../component/SocialUser/SocialUser";
+import { useState } from "react";
+import { IoEye, IoEyeOff } from "react-icons/io5";
+import swal from "sweetalert";
 
 
 const Login = () => {
+    const { handleLogin } = useAuth();
+    const [check, setCheck] = useState(false)
+    const location = useLocation();
+    const navigate = useNavigate();
+
+
+    const handleSignIn = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+
+
+
+        handleLogin(email, password)
+            .then(res => {
+                navigate(location?.state ? location.state : '/')
+                swal(`Welcome Back to Donaty ${res.user.displayName}`, {
+                    buttons: false,
+                    timer: 2000,
+                    className:"bg-green-200"
+                });
+            })
+            .catch(error => {
+                const errorMessage = error.message;
+                if (errorMessage === 'auth/user-not-found') {
+                    swal('User not found with provided email', {
+                        buttons: false,
+                        timer: 2000,
+                        className:"bg-red-200"
+                    });
+
+                } else if (errorMessage === 'auth/wrong-password') {
+                    swal('Incorrect password', {
+                        buttons: false,
+                        timer: 2000,
+                        className:"bg-red-200"
+                    });
+
+                } else {
+                    swal('Your Email or Password is Incorrect', {
+                        buttons: false,
+                        timer: 2000,
+                        className:"bg-red-200"
+                    });
+
+                }
+            })
+
+    }
+
+
+
     return (
-        <div>
-            
+        <div className="hero min-h-screen">
+            <Helmet><title>Login</title></Helmet>
+            <div className="card">
+                <div className="card-body shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleSignIn} className="">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder="email"
+                                className="input input-bordered"
+                                required
+                            />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <div className=" relative">
+                                <input
+                                    type={check ? "text" : "password"}
+                                    name="password"
+                                    placeholder="password"
+                                    className="input input-bordered "
+                                    required
+                                />
+                                <span className="absolute top-1/3 right-3 cursor-pointer" onClick={() => setCheck(!check)}>{check ? <IoEyeOff /> : <IoEye />}</span>
+                            </div>
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button type="submit" className="btn btn-info text-white">Login</button>
+                        </div>
+                        <p className="mt-3 text-sm text-center">Do not have an account? <Link to='/registration' className="hover:underline text-info">Registration</Link></p>
+                    </form>
+                    <SocialUser></SocialUser>
+                </div>
+            </div>
         </div>
     );
 };
